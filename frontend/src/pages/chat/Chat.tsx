7b046596588e5706a3ffc7fd8d80fcfb89fd1e3a
syedahamed <syedahamed@valorpaytech.com>
2022-06-10 09:04:26 +0530

    Login design Updated

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -1,7 +1,6 @@
 import React,{useEffect} from 'react';
 import io  from 'socket.io-client'
-import Box from '@mui/material/Box';
-import {TextField,Button} from '@mui/material';
+
 let socket;
 const Chat = ()=>{
 	const backEndUrl = 'http://localhost:8000/'
@@ -9,19 +8,8 @@ const Chat = ()=>{
 		socket = io(backEndUrl)
 	},[])
 	return(
+	<h1>Hi from Chat</h1>
 	
-	<Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <TextField label="Name" variant="standard" />
-      <TextField label="Password" variant="standard" />
-	  <Button variant="submit">Submit</Button>
-    </Box>
 	
 	)
 }
--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
-
+import Box from '@mui/material/Box';
+import {TextField,Button,Card, FormControl} from '@mui/material';
 const Login = ()=>{
 	
 	return(
-	<h1>Hi From Login</h1>
+		<Card variant="outlined" sx={{position:'absolute',top:'50%',left:'50%',transform: 'translate(-50%, -50%)',width:'500px',padding:'20px'
+	}}>
+			<FormControl sx={{display:'block'}}>
+			<TextField fullWidth label="Name" variant="standard" />
+			</FormControl>
+			<FormControl sx={{display:'block'}}>
+			<TextField fullWidth label="Password" variant="standard" />
+			</FormControl>
+			<FormControl sx={{display:'block',marginTop:'30px'}}>
+			<Button fullWidth variant="contained">Submit</Button>
+			</FormControl>
+			
+		</Card>
+
+		
 	)
 }
 
